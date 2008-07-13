8838d8025d20c34a9c7d96e472b5fe39e200aaeb
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-07-13 16:09:42 -0400

    Don't load plugins_plus when testing

--- test/app_root/config/environment.rb
+++ test/app_root/config/environment.rb
@@ -2,7 +2,7 @@ require 'config/boot'
 
 Rails::Initializer.run do |config|
   config.plugin_paths << '..'
-  config.plugins = %w(plugins_plus tags smart_field_constraints)
+  config.plugins = %w(tags smart_field_constraints)
   config.cache_classes = false
   config.whiny_nils = true
 end
