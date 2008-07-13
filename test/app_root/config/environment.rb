f694ef73b5cd27dec958ce08b3809e094bd496d6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-07-13 16:10:22 -0400

    Remove old additional plugin load paths

--- test/app_root/config/environment.rb
+++ test/app_root/config/environment.rb
@@ -1,7 +1,6 @@
 require 'config/boot'
 
 Rails::Initializer.run do |config|
-  config.plugin_paths << '..'
   config.plugins = %w(tags smart_field_constraints)
   config.cache_classes = false
   config.whiny_nils = true
