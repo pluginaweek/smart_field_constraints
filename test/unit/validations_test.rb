51f908ce87f191b942bb36d9737c29129790b9a6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-10-26 14:48:59 -0400

    Reorganize helper tests

--- test/unit/validations_test.rb
+++ test/unit/validations_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 class ModelWithoutLengthValidationsTest < Test::Unit::TestCase
   def test_should_not_have_any_constraints
