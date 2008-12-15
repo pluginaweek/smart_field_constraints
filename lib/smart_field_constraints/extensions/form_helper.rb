6cabfa17414005b7f53842f4d1bde0c0f4813196
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-12-14 22:34:42 -0500

    Remove the PluginAWeek namespace

--- lib/smart_field_constraints/extensions/form_helper.rb
+++ lib/smart_field_constraints/extensions/form_helper.rb
@@ -1,57 +1,55 @@
-module PluginAWeek #:nodoc:
-  module SmartFieldConstraints
-    module Extensions #:nodoc:
-      # Automatically applies the maximum length for an input field if it can be
-      # determined
-      module InstanceTag
-        def self.included(base) #:nodoc:
-          base.class_eval do
-            alias_method_chain :to_input_field_tag, :smart_constraints
-          end
+module SmartFieldConstraints
+  module Extensions #:nodoc:
+    # Automatically applies the maximum length for an input field if it can be
+    # determined
+    module InstanceTag
+      def self.included(base) #:nodoc:
+        base.class_eval do
+          alias_method_chain :to_input_field_tag, :smart_constraints
         end
-        
-        # Apply constraints for the given field
-        def to_input_field_tag_with_smart_constraints(field_type, options = {})
-          # Only check password and text fields
-          add_max_length_constraints(options) if %w(password text).include?(field_type)
-          
-          to_input_field_tag_without_smart_constraints(field_type, options)
-        end
-        
-        private
-          def add_max_length_constraints(options)
-            options.stringify_keys!
-            return if options['maxlength'] || !object
-            
-            # Look for the attribute's maximum length from tracked validations or
-            # the column's definition
-            if max_length = validation_length || column_limit
-              # If the size isn't specified, use the caller's maxlength of the default value.
-              # This must be done here, otherwise it'll use the maxlength value that
-              # we apply here
-              options['size'] ||= options['maxlength'] || ActionView::Helpers::InstanceTag::DEFAULT_FIELD_OPTIONS['size']
-              options['maxlength'] = max_length
-            end
-          end
-          
-          # Finds the maximum length according to validations
-          def validation_length
-            if constraints = object.class.smart_length_constraints
-              constraints[method_name]
-            end
-          end
-          
-          # Finds the maximum length according to column limits
-          def column_limit
-            if column = object.class.columns_hash[method_name]
-              column.limit
-            end
-          end
       end
+      
+      # Apply constraints for the given field
+      def to_input_field_tag_with_smart_constraints(field_type, options = {})
+        # Only check password and text fields
+        add_max_length_constraints(options) if %w(password text).include?(field_type)
+        
+        to_input_field_tag_without_smart_constraints(field_type, options)
+      end
+      
+      private
+        def add_max_length_constraints(options)
+          options.stringify_keys!
+          return if options['maxlength'] || !object
+          
+          # Look for the attribute's maximum length from tracked validations or
+          # the column's definition
+          if max_length = validation_length || column_limit
+            # If the size isn't specified, use the caller's maxlength of the default value.
+            # This must be done here, otherwise it'll use the maxlength value that
+            # we apply here
+            options['size'] ||= options['maxlength'] || ActionView::Helpers::InstanceTag::DEFAULT_FIELD_OPTIONS['size']
+            options['maxlength'] = max_length
+          end
+        end
+        
+        # Finds the maximum length according to validations
+        def validation_length
+          if constraints = object.class.smart_length_constraints
+            constraints[method_name]
+          end
+        end
+        
+        # Finds the maximum length according to column limits
+        def column_limit
+          if column = object.class.columns_hash[method_name]
+            column.limit
+          end
+        end
     end
   end
 end
 
 ActionView::Helpers::InstanceTag.class_eval do
-  include PluginAWeek::SmartFieldConstraints::Extensions::InstanceTag
+  include SmartFieldConstraints::Extensions::InstanceTag
 end
--- lib/smart_field_constraints/extensions/validations.rb
+++ lib/smart_field_constraints/extensions/validations.rb
@@ -1,76 +1,74 @@
-module PluginAWeek #:nodoc:
-  module SmartFieldConstraints
-    module Extensions #:nodoc:
-      # Tracks validations on the length of fields so that they can be used when
-      # generating form tags for those fields
-      module Validations
-        def self.included(base) #:nodoc:
+module SmartFieldConstraints
+  module Extensions #:nodoc:
+    # Tracks validations on the length of fields so that they can be used when
+    # generating form tags for those fields
+    module Validations
+      def self.included(base) #:nodoc:
+        base.class_eval do
+          extend SmartFieldConstraints::Extensions::Validations::ClassMethods
+        end
+      end
+      
+      module ClassMethods
+        def self.extended(base) #:nodoc:
           base.class_eval do
-            extend PluginAWeek::SmartFieldConstraints::Extensions::Validations::ClassMethods
+            class_inheritable_hash :smart_length_constraints
+            self.smart_length_constraints = {}
+          end
+          
+          class << base
+            alias_method_chain :validates_length_of, :smart_constraints
+            alias_method_chain :validates_size_of, :smart_constraints
           end
         end
         
-        module ClassMethods
-          def self.extended(base) #:nodoc:
-            base.class_eval do
-              class_inheritable_hash :smart_length_constraints
-              self.smart_length_constraints = {}
-            end
-            
-            class << base
-              alias_method_chain :validates_length_of, :smart_constraints
-              alias_method_chain :validates_size_of, :smart_constraints
-            end
-          end
-          
-          # Tracks what the maximum value is that's allowed for all of the
-          # attributes being validated
-          def validates_length_of_with_smart_constraints(*attrs)
-            track_length_constraints(attrs)
-            validates_length_of_without_smart_constraints(*attrs)
-          end
-          
-          # Tracks what the maximum value is that's allowed for all of the
-          # attributes being validated
-          def validates_size_of_with_smart_constraints(*attrs)
-            track_length_constraints(attrs)
-            validates_size_of_without_smart_constraints(*attrs)
-          end
-          
-          private
-            def track_length_constraints(attrs)
-              options = attrs.last
-              if options.is_a?(Hash)
-                # Extract the option restricting the length
-                options = options.symbolize_keys
-                range_options = ActiveRecord::Validations::ClassMethods::ALL_RANGE_OPTIONS & options.keys
-                option = range_options.first
-                option_value = options[range_options.first]
-                
-                # Find the max value from ranges or specific maximums
-                max_length = nil
-                case option
-                when :within, :in
-                  max_length = option_value.end
-                when :maximum, :is
-                  max_length = option_value
-                end
-                
-                if max_length
-                  # Store the maximum value for each attribute so that it can be referenced later
-                  attrs.each do |attr|
-                    self.smart_length_constraints ||= {}
-                    self.smart_length_constraints[attr.to_s] = max_length
-                  end
+        # Tracks what the maximum value is that's allowed for all of the
+        # attributes being validated
+        def validates_length_of_with_smart_constraints(*attrs)
+          track_length_constraints(attrs)
+          validates_length_of_without_smart_constraints(*attrs)
+        end
+        
+        # Tracks what the maximum value is that's allowed for all of the
+        # attributes being validated
+        def validates_size_of_with_smart_constraints(*attrs)
+          track_length_constraints(attrs)
+          validates_size_of_without_smart_constraints(*attrs)
+        end
+        
+        private
+          def track_length_constraints(attrs)
+            options = attrs.last
+            if options.is_a?(Hash)
+              # Extract the option restricting the length
+              options = options.symbolize_keys
+              range_options = ActiveRecord::Validations::ClassMethods::ALL_RANGE_OPTIONS & options.keys
+              option = range_options.first
+              option_value = options[range_options.first]
+              
+              # Find the max value from ranges or specific maximums
+              max_length = nil
+              case option
+              when :within, :in
+                max_length = option_value.end
+              when :maximum, :is
+                max_length = option_value
+              end
+              
+              if max_length
+                # Store the maximum value for each attribute so that it can be referenced later
+                attrs.each do |attr|
+                  self.smart_length_constraints ||= {}
+                  self.smart_length_constraints[attr.to_s] = max_length
                 end
               end
             end
-        end
+          end
       end
     end
   end
 end
 
 ActiveRecord::Base.class_eval do
-  include PluginAWeek::SmartFieldConstraints::Extensions::Validations
+  include SmartFieldConstraints::Extensions::Validations
 end
