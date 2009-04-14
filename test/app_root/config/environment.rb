91176cc1770e2cec1d038a1ddb7668541ebff51b
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2009-04-13 21:19:26 -0400

    Update test environment to work with the latest plugin_test_helper

--- test/app_root/config/environment.rb
+++ test/app_root/config/environment.rb
@@ -4,4 +4,5 @@ Rails::Initializer.run do |config|
   config.plugins = %w(tags smart_field_constraints)
   config.cache_classes = false
   config.whiny_nils = true
+  config.action_controller.session = {:key => 'rails_session', :secret => 'd229e4d22437432705ab3985d4d246'}
 end
